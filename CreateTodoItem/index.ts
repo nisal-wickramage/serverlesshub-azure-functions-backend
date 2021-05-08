0aaff587f4101f9042a18c38cb45f340d4c74dc0
nisal-wickramage <nisalwickramage@gmail.com>
2021-05-08 14:49:40 +0800

    Add input validation for create todo item.

--- CreateTodoItem/index.ts
+++ CreateTodoItem/index.ts
@@ -1,13 +1,29 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import { NewTodoItem } from "./Models/new-todo-item";
+import { TodoItem } from "./Models/todo-item";
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    context.log('HTTP trigger function processed a request.');
+    context.log.info('Create todo item started.');
 
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: req.body
-    };
+    const todoItem = req.body as NewTodoItem;
 
+    if(todoItem && todoItem.title && todoItem.description){
+        context.res = {
+            status: 200,
+            body: {
+                id:'dummy-id',
+                title: todoItem.title,
+                descrition: todoItem.description
+            } as TodoItem
+        }
+    } else {
+        context.res = {
+            status: 400
+        }
+        context.log.error('Create todo item invalid item.', context.executionContext, JSON.stringify(todoItem));
+    }
+    context.done();
+    context.log.info('Create todo item completed.');
 };
 
 export default httpTrigger;
